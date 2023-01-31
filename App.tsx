0455d0dee59b672c6d86a9d44ba1eef6fdfee5c4
forinda <55891685+forinda@users.noreply.github.com>
2023-01-31 20:21:24 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+import { FancyButton } from "react_button_component_demo";
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <FancyButton title="hello world" />
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-c25tsr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-c25tsr)
